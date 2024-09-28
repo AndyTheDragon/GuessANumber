82afd2c0e566cbc661b877ec973d1ff4789501eb
AndyTheDragon <andre.jansam@gmail.com>
2024-09-29 00:10:12 +0200

    Lvl2 versionen

---
+++ src/main/java/org/example/LocalUI.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class LocalUI implements UI
+{
+    private Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public int getUserInput()
+    {
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            try {
+                int output = Integer.parseInt(input);
+                return output;
+            }
+            catch (NumberFormatException e) {
+                sendMsg("Please input an integer");
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void sendMsg(String msg) {
+        System.out.println(msg);
+    }
+}
---
+++ src/main/java/org/example/UI.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface UI
+{
+    int getUserInput();
+    //int getIntInput();
+    //String getStringInput();
+
+    void sendMsg(String msg);
+}

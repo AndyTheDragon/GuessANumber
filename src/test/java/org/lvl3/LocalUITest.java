776141a0fa9557982b7368bd52ea9bc62a07d984
AndyTheDragon <andre.jansam@gmail.com>
2024-09-29 22:22:49 +0200

    Test af getUserInput()

---
+++ src/test/java/org/lvl3/LocalUITest.java
@@ -0,0 +1,58 @@
+package org.lvl3;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalUITest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void getUserInput_validInput()
+    {
+        //Arrange
+        int expected = 42;
+        String input = "42\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        LocalUI localUI = new LocalUI(in);
+
+        //Act
+        int result = localUI.getUserInput();
+
+        //Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void getUserInput_invalidInput()
+    {
+        //Arrange
+        int expected = 42;
+        String input = "a\n4.2\nb\n42\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        LocalUI localUI = new LocalUI(in);
+
+        //Act
+        int result = localUI.getUserInput();
+
+        //Assert
+        assertEquals(expected, result);
+
+    }
+}

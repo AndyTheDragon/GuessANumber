da46a6a86e364f3f510d276c08b0a574fc9f0b82
AndyTheDragon <andre.jansam@gmail.com>
2024-09-29 22:23:21 +0200

    Hvordan skal man teste private metoder? Skal de måske være public?

---
+++ src/test/java/org/lvl2/GuessANumberGameTest.java
@@ -0,0 +1,16 @@
+package org.lvl2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuessANumberGameTest
+{
+
+    @Test
+    void giveFeedbackTest() {
+        // Arrange
+        GuessANumberGame game = new GuessANumberGame();
+
+    }
+}

e9168ecea9a1400d4d3dd2e084eae74532d1611b
AndyTheDragon <andre.jansam@gmail.com>
2024-09-30 15:46:29 +0200

    removed unused import

--- src/test/java/org/lvl3/LocalUITest.java
+++ src/test/java/org/lvl3/LocalUITest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
